620cb9adf71aeb3ba2b2332e76b0d40b31836f0d
GreRut <ruta.grzegorz@gmail.com>
2024-11-29 16:48:10 +0100

    build: implement CSS for mobile

--- ReactiveFolio/src/App.jsx
+++ ReactiveFolio/src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -9,62 +7,62 @@ function App() {
 return (
 	
 	<>
-		<nav>
-			<a href className="text-3xl font-bold underline">
+		<nav class="navbar">
+			<a href className="text-base text-stone-100">
 				Home
 			</a>
-			<a href className="text-3xl font-bold underline">
+			<a href className="text-base text-stone-100">
 				About
 			</a>
-			<a href className="text-3xl font-bold underline">
+			<a href className="text-base text-stone-100">
 				Projects
 			</a>
 		{/* Maybe put the buttons here and keep hidden? */}
 		</nav>
 		<main>
 			<section>
-				<img src={viteLogo} className="logo" alt="Vite logo" />
-				<h1>
+				<img src="profilePic.png" className="mx-auto size-6/12" alt="Gregor's profile picture"  />
+				<h1 class="paragraphs">
 					Gregor Ruta
 				</h1>
-				<p>
-					Summary: I am very good boy, please hire me, I have approximate knowledge of many things. I can frontend backend, sometimes I can lift heavy things.
+				<p className="text-justify my-5 mx-auto leading-5">
+				I am a full-stack C#/.NET developer based in Stockholm, Sweden, with a background in construction project management. I specialize in .NET framework, utilizing my experience with Entity Framework, and Azure. I am curious, driven, and versatile with other programming languages, such as Python and C/C++, which I have applied in data management and computer vision projects.
 				</p>
-				<article>
+				<article class = "social">
 					<img src={viteLogo} className="logo" alt="Vite logo" />
 					<img src={viteLogo} className="logo" alt="Vite logo" />
 					<img src={viteLogo} className="logo" alt="Vite logo" />
 					<img src={viteLogo} className="logo" alt="Vite logo" />
 				</article>
-				<article>
+				<article class = "buttons">
 					<button> Hire </button>
 					<button> Resume </button>
 				</article>
 			</section>
 			<section>
-				<h1>
+				<h1 class="paragraphs">
 					About
 				</h1>
 
-				<h2>
+				<h2 class="subparagraphs" Name="text-left text-sky-600 text-xl font-semibold my-3">
 					Education
 				</h2>
-				<ul>
+				<ul class="lists">
 					<li>Baldur's Gate University</li>
 					<li>Candlekeep Academy</li>
 				</ul>
-				<h2>
+				<h2 class="subparagraphs">
 					Work
 				</h2>
-				<ul>
+				<ul class="lists">
 					<li>Cloakwood Forest</li>
 					<li>Feldepost Inn</li>
 					<li>Nashkel Mines</li>
 				</ul>
-				<h2>
+				<h2 class="subparagraphs">
 					Skills
 				</h2>
-				<article>
+				<article class="skills">
 					<img src={viteLogo} className="logo" alt="Vite logo" />
 					<img src={viteLogo} className="logo" alt="Vite logo" />
 					<img src={viteLogo} className="logo" alt="Vite logo" />
@@ -80,20 +78,22 @@ return (
 				</article>
 			</section>
 			<section>
-				<h1>
+				<h1 class = "paragraphs">
 					Projects
 				</h1>
 					<article>
-					<h2>
+					<h2 class="subparagraphs">
 						Nice Project
 					</h2>
-					<ul>
+					<ul class="lists">
 						<li>Place: London</li>
 						<li>Space: Sqaure meters</li>
 					</ul>
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-					<img src={viteLogo} className="logo" alt="Vite logo" />
+					<article class ="project-portfolio">
+						<img src="images.jpg" alt="an illustration for Nice Project" class="portfolio-picture"/>
+						<img src="images.jpg" alt="an illustration for Nice Project" class="portfolio-picture"/>
+						<img src="images.jpg" alt="an illustration for Nice Project" class="portfolio-picture"/>
+					</article>
 				</article>
 			</section>
 		</main>
