bfe05456f35d393056eabe2e30777735eaf10fad
GreRut <ruta.grzegorz@gmail.com>
2024-12-01 19:17:24 +0100

    build: implement css for tablet

--- ReactiveFolio/src/App.jsx
+++ ReactiveFolio/src/App.jsx
@@ -8,26 +8,30 @@ return (
 	
 	<>
 		<nav class="navbar">
-			<a href className="text-base text-stone-100">
+			<a href className="text-base text-stone-100 bg-sky-600">
 				Home
 			</a>
-			<a href className="text-base text-stone-100">
+			<a href className="text-base text-stone-100  bg-sky-600">
 				About
 			</a>
-			<a href className="text-base text-stone-100">
+			<a href className="text-base text-stone-100  bg-sky-600">
 				Projects
 			</a>
 		{/* Maybe put the buttons here and keep hidden? */}
 		</nav>
 		<main>
-			<section>
-				<img src="profilePic.png" className="mx-auto size-6/12" alt="Gregor's profile picture"  />
+			<section class="landing">
+				<section class ="profile-pic">
+				<img src="profilePic.png"  alt="Gregor's profile picture"  />
+				</section>
+				<section>
 				<h1 class="paragraphs">
 					Gregor Ruta
 				</h1>
-				<p className="text-justify my-5 mx-auto leading-5">
+				<p  class="lead">
 				I am a full-stack C#/.NET developer based in Stockholm, Sweden, with a background in construction project management. I specialize in .NET framework, utilizing my experience with Entity Framework, and Azure. I am curious, driven, and versatile with other programming languages, such as Python and C/C++, which I have applied in data management and computer vision projects.
 				</p>
+				</section>
 				<article class = "social">
 					<img src={viteLogo} className="logo" alt="Vite logo" />
 					<img src={viteLogo} className="logo" alt="Vite logo" />
@@ -39,48 +43,63 @@ return (
 					<button> Resume </button>
 				</article>
 			</section>
-			<section>
-				<h1 class="paragraphs">
-					About
-				</h1>
+			<section class = "CV">
+				<article>
+					<h1 class="paragraphs">
+						About
+					</h1>
+				</article>
+				<article>
 
-				<h2 class="subparagraphs" Name="text-left text-sky-600 text-xl font-semibold my-3">
+				</article>
+				<article>
+				<h2 class="subparagraphs">
 					Education
 				</h2>
 				<ul class="lists">
 					<li>Baldur's Gate University</li>
 					<li>Candlekeep Academy</li>
 				</ul>
-				<h2 class="subparagraphs">
-					Work
-				</h2>
-				<ul class="lists">
-					<li>Cloakwood Forest</li>
-					<li>Feldepost Inn</li>
-					<li>Nashkel Mines</li>
-				</ul>
-				<h2 class="subparagraphs">
-					Skills
-				</h2>
-				<article class="skills">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</article>
+				<article>
+					<h2 class="subparagraphs">
+						Work
+					</h2>
+					<ul class="lists">
+						<li>Cloakwood Forest</li>
+						<li>Feldepost Inn</li>
+						<li>Nashkel Mines</li>
+					</ul>
+				</article>
+				<article>
+					<h2 class="subparagraphs">
+						Skills
+					</h2>
+					<article class="skills">
+						<img src={viteLogo} className="logo" alt="Vite logo" />
+						<img src={viteLogo} className="logo" alt="Vite logo" />
+						<img src={viteLogo} className="logo" alt="Vite logo" />
+						<img src={viteLogo} className="logo" alt="Vite logo" />
+						<img src={viteLogo} className="logo" alt="Vite logo" />
+						<img src={viteLogo} className="logo" alt="Vite logo" />
+						<img src={viteLogo} className="logo" alt="Vite logo" />
+						<img src={viteLogo} className="logo" alt="Vite logo" />
+						<img src={viteLogo} className="logo" alt="Vite logo" />
+						<img src={viteLogo} className="logo" alt="Vite logo" />
+						<img src={viteLogo} className="logo" alt="Vite logo" />
+						<img src={viteLogo} className="logo" alt="Vite logo" />
+					</article>
 				</article>
 			</section>
-			<section>
-				<h1 class = "paragraphs">
-					Projects
-				</h1>
+			<section class ="portfolio">
+				<article>
+					<h1 class = "paragraphs">
+						Projects
+					</h1>
+				</article>
+				<article>
+
+				</article>
 					<article>
 					<h2 class="subparagraphs">
 						Nice Project
@@ -89,11 +108,15 @@ return (
 						<li>Place: London</li>
 						<li>Space: Sqaure meters</li>
 					</ul>
-					<article class ="project-portfolio">
-						<img src="images.jpg" alt="an illustration for Nice Project" class="portfolio-picture"/>
-						<img src="images.jpg" alt="an illustration for Nice Project" class="portfolio-picture"/>
-						<img src="images.jpg" alt="an illustration for Nice Project" class="portfolio-picture"/>
-					</article>
+				</article>
+				<article class ="project-portfolio">
+					<img src="images.jpg" alt="an illustration for Nice Project" class="portfolio-picture"/>
+				</article>
+				<article>
+					<img src="images.jpg" alt="an illustration for Nice Project" class="portfolio-picture"/>
+				</article>
+				<article>
+					<img src="images.jpg" alt="an illustration for Nice Project" class="portfolio-picture"/>
 				</article>
 			</section>
 		</main>
